336413249bd6b28035b373e97abb4c09ea04b97f
Brett Henderson <bretth18@gmail.com>
2025-12-13 15:30:40 -0800

    Initial Commit

---
+++ abledex/abledexApp.swift
@@ -0,0 +1,17 @@
+//
+//  abledexApp.swift
+//  abledex
+//
+//  Created by Brett Henderson on 12/13/25.
+//
+
+import SwiftUI
+
+@main
+struct abledexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
